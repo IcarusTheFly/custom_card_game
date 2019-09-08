5c60141370e74fc3d739dcfb79319d394204c232
IcarusTheFly <IcarusTheFly@users.noreply.github.com>
2019-09-08 17:07:40 +0200

    Updates

---
+++ precache-manifest.dfa3572485c1c5907e1ef812c2739b23.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99a945d88162cca2b1653fd8f52b736e",
+    "url": "/custom_card_game/index.html"
+  },
+  {
+    "revision": "cc034cfb1e682725ac41",
+    "url": "/custom_card_game/static/css/2.a020186b.chunk.css"
+  },
+  {
+    "revision": "112a1588f9194c69c8f9",
+    "url": "/custom_card_game/static/css/main.52de40cd.chunk.css"
+  },
+  {
+    "revision": "cc034cfb1e682725ac41",
+    "url": "/custom_card_game/static/js/2.a55862f8.chunk.js"
+  },
+  {
+    "revision": "112a1588f9194c69c8f9",
+    "url": "/custom_card_game/static/js/main.abf6fac2.chunk.js"
+  },
+  {
+    "revision": "f477bcc6d339dfc7482e",
+    "url": "/custom_card_game/static/js/runtime~main.721ce15f.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/custom_card_game/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/custom_card_game/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/custom_card_game/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/custom_card_game/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/custom_card_game/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
